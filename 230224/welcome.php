16d3b21357b589a2f36821361b79d8115a3f0816
SPirate9 <118197355+SPirate9@users.noreply.github.com>
2023-04-14 18:22:32 +0200

    Update welcome.php

--- 230224/welcome.php
+++ 230224/welcome.php
@@ -13,19 +13,18 @@ if (isset($_SESSION["userEmail"])) { ?>
     <h3>Bienvenue <?= $_SESSION["userName"] ?></h3>
     <p>Votre E-mail est : <?= $_SESSION["userEmail"] ?></p>
     <form method="post">
-        <label>Nouveau Tweet :  </label>
+        <label>Nouveau Tweet : </label>
         <input type="text" name="content" />
         <button type="submit" name="post">Tweeter</button>
         <?php if (isset($_POST["post"])) {
             if (empty($_POST["content"])) {
-                echo " ";
-                header("Refresh: 0; URL= welcome.php");
-                exit();
+                 echo "<p style='color:red'>Le tweet ne peut pas être vide</p>";
             } else {
                 $insert = $conn->prepare(
                     "INSERT INTO posts ( content , userName ) VALUES (?,?)"
                 );
                 if ($insert->execute([$_POST["content"],$_SESSION["userName"]])) {
+		 echo "<p style='color:green'>Le tweet a été publié avec succès</p>";
                 }
             }
         } ?>
@@ -35,14 +34,27 @@ if (isset($_SESSION["userEmail"])) { ?>
     $display->execute();
     $data = $display->fetchAll();
     foreach ($data as $post) {
-        echo "
-        <div>
-            {$post["content"]} :  
-            {$post["created_at"]} Posté par 
-	    {$post["userName"]}
-        </div>
-      
-		";
+	$postContent = $post["content"];
+        $postDate = $post["created_at"];
+        $postAuthor = $post["userName"];
+        $postId = $post['ID'];
+	    
+	     if ($postAuthor === $_SESSION["userName"]) {
+            echo "<div>{$postContent}: {$postDate} Posté par {$postAuthor}
+                <form method='post'>
+                    <input type='hidden' name='postId' value='{$postId}' />
+                    <button type='submit' name='delete'>Supprimer</button>
+                </form>
+            </div>";
+        } else {
+            echo "<div>{$postContent}: {$postDate} Posté par {$postAuthor}</div>";
+        }
+    }
+    if (isset($_POST["delete"])) {
+        $delete = $conn->prepare("DELETE FROM posts WHERE ID = ? AND userName = ?");
+        $delete->execute([$_POST["postId"], $_SESSION["userName"]]);
+        header("Refresh: 0; URL= welcome.php");
+        exit();
     }
     ?>
   </p>
@@ -52,4 +64,4 @@ if (isset($_SESSION["userEmail"])) { ?>
 </html>
 <?php } else {echo "Veuillez vous connecter pour accéder à votre compte redirection automatique au bout de 5 secondes";
     header("Refresh: 5;URL=index.php");}
-?>
+
